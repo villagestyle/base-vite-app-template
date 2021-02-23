1de12f56b03f6d5121770957226fca480e63606a
villagestyle <1044223679@qq.com>
2021-02-23 16:29:40 +0800

    init

---
+++ src/views/home/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <h3>{{ pageName }}</h3>
+</template>
+
+<script>
+import { AppStore } from '../../store/modules/app'
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  mounted() {
+    console.log(AppStore.getSysNo);
+  },
+  setup() {
+    const pageName = 'Home Page';
+    
+    return {
+      pageName
+    }
+  }
+})
+
+</script>
